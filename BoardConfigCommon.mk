27cd79d34746fb828eeebe7b503b41f8008f9fe4
drgroovestarr <jarrod.worlitz@gmail.com>
2016-01-15 14:14:30 -0800

    Enable dex preoptimization

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,6 +71,16 @@ COMMON_GLOBAL_CFLAGS += \
 BOARD_HARDWARE_CLASS := device/lge/g2-common/cmhw/
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/touch_gesture"
 
+# Dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+DONT_DEXPREOPT_PREBUILTS := true
+
 # Display
 HAVE_ADRENO_SOURCE := false
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
